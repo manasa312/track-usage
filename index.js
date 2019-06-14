e16d36c1fc0770ef6e9c73dd7fb567d5f1569c9c
manasa312 <51831844+manasa312@users.noreply.github.com>
2019-06-14 22:12:29 +0530

    Implementation

---
+++ index.js
@@ -0,0 +1,511 @@
+var softwares = [{
+    "id": "1",
+    "name": "adp",
+    "category": "4",
+    "globalUsage": 10,
+    "searches": 10,
+    "rating": 4,
+    "views":100,
+    "reviews": 50,
+    "globalReview": 10
+}, {
+    "id": "2",
+    "name": "PayChex Flex",
+    "category": "4",
+    "globalUsage": 20,
+    "searches": 10,
+    "rating": 2,
+    "views": 299,
+    "reviews": 100,
+    "globalReview": 10
+}, {
+    "id": "3",
+    "name": "Paylocity",
+    "category": "4",
+    "globalUsage": 30,
+    "searches": 10,
+    "rating": 3,
+    "views": 102,
+    "reviews": 70,
+    "globalReview": 10
+}, {
+    "id": "4",
+    "name": "Paycom",
+    "category": "4",
+    "globalUsage": 40,
+    "searches": 10,
+    "reviews": 190,
+    "rating": 3.2,
+    "views": 106,
+    "globalReview": 10
+}, {
+    "id": "5",
+    "name": "PayChex Flex",
+    "category": "4",
+    "globalUsage": 50,
+    "searches": 10,
+    "reviews": 180,
+    "rating": 3.1,
+    "views": 120,
+    "globalReview": 10
+}, {
+    "id": "6",
+    "name": "Workday HCM",
+    "category": "4",
+    "globalUsage": 60,
+    "searches": 10,
+    "reviews": 210,
+    "rating": 3.6,
+    "views": 140,
+    "globalReview": 10
+}, {
+    "id": "7",
+    "name": "PeopleSoft",
+    "category": "4",
+    "globalUsage": 200,
+    "searches": 10,
+    "reviews": 98,
+    "rating": 3.7,
+    "views": 170,
+    "globalReview": 10
+}, {
+    "id": "8",
+    "name": "Trinet",
+    "category": "4",
+    "globalUsage": 150,
+    "searches": 10,
+    "reviews": 80,
+    "rating": 4,
+    "views": 190,
+    "globalReview": 10
+}, {
+    "id": "9",
+    "name": "Rippling",
+    "category": "4",
+    "globalUsage": 70,
+    "searches": 10,
+    "reviews": 150,
+    "rating": 4.1,
+    "views": 200,
+    "globalReview": 10
+}, {
+    "id": "10",
+    "name": "ClearCompany",
+    "category": "4",
+    "globalUsage": 250,
+    "searches": 10,
+    "reviews": 180,
+    "rating": 4.2,
+    "views": 232,
+    "globalReview": 10
+}, {
+    "id": "11",
+    "name": "Oracle Talent Management",
+    "category": "4",
+    "globalUsage": 300,
+    "searches": 10,
+    "reviews": 110,
+    "rating": 4.5,
+    "views": 150,
+    "globalReview": 10
+}, {
+    "id": "12",
+    "name": "FingerCheck",
+    "category": "4",
+    "globalUsage": 80,
+    "searches": 310,
+    "reviews": 10,
+    "rating": 2.7,
+    "views": 260,
+    "globalReview": 10
+}, {
+    "id": "13",
+    "name": "Domo",
+    "category": "3",
+    "globalUsage": 160,
+    "searches": 10,
+    "reviews": 410,
+    "rating": 3,
+    "views": 210,
+    "globalReview": 10
+}, {
+    "id": "14",
+    "name": "MicroStrategy",
+    "category": "3",
+    "globalUsage": 10,
+    "searches": 210,
+    "reviews": 210,
+    "rating": 3.4,
+    "views": 241,
+    "globalReview": 10
+}, {
+    "id": "15",
+    "name": "Looker",
+    "category": "3",
+    "globalUsage": 90,
+    "searches": 10,
+    "reviews": 390,
+    "rating": 3.1,
+    "views": 249,
+    "globalReview": 10
+}, {
+    "id": "16",
+    "name": "SAP Lumira",
+    "category": "3",
+    "globalUsage": 310,
+    "searches": 10,
+    "reviews": 156,
+    "rating": 2.7,
+    "views": 190,
+    "globalReview": 10
+}, {
+    "id": "17",
+    "name": "Google Chart Tools",
+    "category": "3",
+    "globalUsage": 100,
+    "searches": 10,
+    "reviews": 100,
+    "rating": 3.4,
+    "views": 250,
+    "globalReview": 10
+}, {
+    "id": "18",
+    "name": "Grow.com",
+    "category": "3",
+    "globalUsage": 20,
+    "searches": 10,
+    "reviews": 90,
+    "rating": 3.2,
+    "views": 152,
+    "globalReview": 10
+}, {
+    "id": "19",
+    "name": "Databox",
+    "category": "3",
+    "globalUsage": 85,
+    "searches": 10,
+    "reviews": 70,
+    "rating": 3.3,
+    "views": 99,
+    "globalReview": 10
+}, {
+    "id": "20",
+    "name": "Logi Analytics",
+    "category": "3",
+    "globalUsage": 70,
+    "searches": 10,
+    "reviews": 90,
+    "rating": 4.3,
+    "views": 72,
+    "globalReview": 10
+}, {
+    "id": "21",
+    "name": "Sisense",
+    "category": "3",
+    "globalUsage": 180,
+    "searches": 10,
+    "reviews": 130,
+    "rating": 3.1,
+    "views": 142,
+    "globalReview": 10
+}, {
+    "id": "22",
+    "name": "dynatrace",
+    "category": "2",
+    "globalUsage":230,
+    "searches": 10,
+    "reviews": 120,
+    "rating": 3.7,
+    "views": 312,
+    "globalReview": 10
+}, {
+    "id": "23",
+    "name": "Microsoft Service Center",
+    "category": "2",
+    "globalUsage": 280,
+    "searches": 10,
+    "reviews": 180,
+    "rating": 3.4,
+    "views": 224,
+    "globalReview": 10
+}, {
+    "id": "24",
+    "name": "AppDynamics",
+    "category": "2",
+    "globalUsage": 290,
+    "searches": 10,
+    "reviews": 350,
+    "rating": 3.4,
+    "views": 172,
+    "globalReview": 10
+}, {
+    "id": "25",
+    "name": "Oracle Enterprise Manager",
+    "category": "2",
+    "globalUsage": 200,
+    "searches": 10,
+    "reviews": 190,
+    "rating": 3.3,
+    "views": 142,
+    "globalReview": 10
+},{
+    "id": "26",
+    "name": "DataDog",
+    "category": "2",
+    "globalUsage": 300,
+    "searches": 10,
+    "reviews": 140,
+    "rating": 4.2,
+    "views": 152,
+    "globalReview": 10
+}, {
+    "id": "27",
+    "name": "Sumo Logic",
+    "category": "2",
+    "globalUsage": 130,
+    "searches": 10,
+    "reviews": 170,
+    "rating": 2.3,
+    "views": 82,
+    "globalReview": 10
+}, {
+    "id": "28",
+    "name": "Pulse Secure Virtual Traffic Manager",
+    "category": "2",
+    "globalUsage": 410,
+    "searches": 10,
+    "reviews": 180,
+    "rating": 3.5,
+    "views": 142,
+    "globalReview": 10
+}, {
+    "id": "29",
+    "name": "Instana",
+    "category": "2",
+    "globalUsage": 85,
+    "searches": 10,
+    "reviews": 210,
+    "rating": 3.3,
+    "views": 182,
+    "globalReview": 10
+}, {
+    "id": "30",
+    "name": "LogicMonitor",
+    "category": "2",
+    "globalUsage": 195,
+    "searches": 10,
+    "reviews": 156,
+    "rating": 3.4,
+    "views": 112,
+    "globalReview": 10
+}, {
+    "id": "31",
+    "name": "Splunk Cloud",
+    "category": "2",
+    "globalUsage": 275,
+    "searches": 10,
+    "reviews": 310,
+    "rating": 3.6,
+    "views": 168,
+    "globalReview": 10
+}, {
+    "id": "32",
+    "name": "MultiView",
+    "category": "1",
+    "globalUsage": 289,
+    "searches": 10,
+    "rating": 3.7,
+    "views": 312,
+    "reviews": 172,
+    "globalReview": 10
+}, {
+    "id": "33",
+    "name": "EmailMagpi",
+    "category": "1",
+    "globalUsage": 430,
+    "searches": 10,
+    "reviews": 129,
+    "rating": 3.2,
+    "views": 50,
+    "globalReview": 10
+}, {
+    "id": "34",
+    "name": "SmartBug Media",
+    "category": "1",
+    "globalUsage": 206,
+    "searches": 10,
+    "reviews": 110,
+    "rating": 3.8,
+    "views": 65,
+    "globalReview": 10
+}, {
+    "id": "35",
+    "name": "EarthLink",
+    "category": "1",
+    "globalUsage": 179,
+    "searches": 10,
+    "reviews": 170,
+    "rating": 3.5,
+    "views": 132,
+    "globalReview": 10
+}, {
+    "id": "36",
+    "name": "GlobalWebIndex",
+    "category": "1",
+    "globalUsage": 430,
+    "searches": 10,
+    "rating": 3.2,
+    "views": 162,
+    "reviews": 410,
+    "globalReview": 10
+}, {
+    "id": "37",
+    "name": "SRDS",
+    "category": "1",
+    "globalUsage": 360,
+    "searches": 10,
+    "reviews": 290,
+    "rating": 3.5,
+    "views": 132,
+    "globalReview": 10
+}, {
+    "id": "38",
+    "name": "DemandGen",
+    "category": "1",
+    "globalUsage": 185,
+    "searches": 10,
+    "reviews": 200,
+    "rating": 3.3,
+    "views": 126,
+    "globalReview": 10
+}, {
+    "id": "39",
+    "name": "MVFGlobal",
+    "category": "1",
+    "globalUsage": 109,
+    "searches": 10,
+    "rating": 2.9,
+    "views": 82,
+    "reviews": 150,
+    "globalReview": 10
+}, {
+    "id": "40",
+    "name": "6Minded",
+    "category": "1",
+    "globalUsage": 209,
+    "searches": 10,
+    "reviews": 280,
+    "rating": 3.3,
+    "views": 192,
+    "globalReview": 10
+}, {
+    "id": "41",
+    "name": "AmberLeaf",
+    "category": "1",
+    "globalUsage": 201,
+    "searches": 10,
+    "reviews": 290,
+    "rating": 3.1,
+    "views": 162,
+    "globalReview": 10
+}]
+var roles = [{
+    "id": "1",
+    "name": "Marketing"
+}, {
+    "id": "2",
+    "name": "Developer"
+}, {
+    "id": "3",
+    "name": "Analyst"
+}, {
+    "id": "4",
+    "name": "HR"
+}]
+
+var app = angular.module('growthGraph', [])
+var trending = ['40', '24', '16', '4', '19', '10', '8', '36']
+var calculateTrending = function() {
+    return trending //logic for returing trending softwares
+}
+app.controller('userController', function($scope) { 
+    $scope.roles = roles   
+   
+    $scope.propMap = {}
+    $scope.value
+    $scope.init = function() {
+        $scope.selectedName = "Please Select"
+        $scope.showTable = true
+        $scope.tableName = "Trending Softwares"
+        $scope.showAll = false
+        calculateTrending()
+        $scope.targetSoftware = []
+        var trending = calculateTrending()
+        for(var i=0; i< trending.length; i++) {
+            $scope.targetSoftware.push(softwares[trending[i]-1])
+        }
+    }
+    $scope.selectedDep = function() {
+        $scope.showAll = false
+        $scope.showTable = true
+        $scope.details = false
+        $scope.tableName = roles[$scope.selectedName-1].name + " Softwares"
+        $scope.targetSoftware = []
+        for(var i=0;i<softwares.length;i++) {
+            if(softwares[i].category == $scope.selectedName){
+                $scope.targetSoftware.push(softwares[i])
+            }
+        }
+        $scope.value = $scope.selectedName
+    }
+
+    $scope.itemClick = function(event) {
+        $scope.showTable = false
+        $scope.details = true
+        $scope.tableName = softwares[event.target.id-1].name + " Software Deatils"
+        softwares[event.target.id-1].views = softwares[event.target.id-1].views  +1
+        $scope.selectedSoftware = softwares[event.target.id-1]
+    }
+
+    $scope.clickBack = function(event) {
+        if($scope.value && $scope.value != -1) {
+            $scope.tableName = roles[$scope.value-1].name + " Softwares"
+        } else if($scope.showAll) {
+            $scope.tableName = "All softwares"
+        } else $scope.tableName = "Trending Softwares"
+        $scope.showTable = true
+        $scope.details = false
+    }
+    $scope.sortClicked = function(prop) {
+        var p = $scope.propMap[prop]
+        if(prop == 'name') {
+            $scope.targetSoftware.sort(function(x,y) {
+                var a = x[prop].toLowerCase()
+                var b = y[prop].toLowerCase()
+                if(p) {
+                    if(a>b) return 1
+                    if(a<b) return -1
+                } else {
+                    if(a<b) return 1
+                    if(a>b) return -1
+                }
+          
+                return 0
+            })
+        } else {
+            $scope.targetSoftware.sort(function(x,y) {
+                return p ? x[prop]-y[prop] : y[prop]-x[prop]
+            })
+        }
+      
+        $scope.propMap[prop] = !p
+    }
+    $scope.showAllSoftwares = function() {
+        $scope.showAll = true
+        $scope.showTable = true
+        $scope.details = false
+        $scope.targetSoftware = softwares
+        $scope.selectedName = $scope.value = -1
+        $scope.tableName = "All softwares"
+    }
+})
